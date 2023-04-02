5e30024d01fe421954f5807fe45721c0a0eca678
adhikari1983 <91963837+adhikari1983@users.noreply.github.com>
2023-04-02 17:49:30 -0400

    class work 8

--- src/class8_loop_revision/E8BreakExample.java
+++ src/class8_loop_revision/E8BreakExample.java
@@ -1,10 +1,12 @@
+/*   Create a  boolean variable summer store true in it,  then write a loop
+     that runs as long as it is summer. Create a temp variable store 85 and
+     inside the loop check the temp if it is less than 100 print "Its good I enjoy summer",
+     as soon as temperature exceeds 100 print "It's very hot " and break the loop with
+     break keyword. */
 package class8_loop_revision;
 
 public class E8BreakExample {
     public static void main(String[] args) {
-        /* create a boolean variable summer store true in it then write a loop that runs as long as it is summer
-           create
-         */
         boolean summer = true;
         int temperature = 105;
 
