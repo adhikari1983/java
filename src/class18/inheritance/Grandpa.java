3dc5bc3440e615632574361d309af5bd2f655c00
adhikari1983 <91963837+adhikari1983@users.noreply.github.com>
2023-05-13 10:27:42 -0400

    class 18 example

---
+++ src/class18/inheritance/Grandpa.java
@@ -0,0 +1,28 @@
+package class18.inheritance;
+
+public class Grandpa {
+    double money = 150;
+
+}
+
+class Father extends  Grandpa{
+   double money = 100;           // firstly, behind the exactly same properties are copied from grandpa class
+}
+
+class Child extends Father{
+    void printMoney(){             // firstly, behind the exactly same properties are copied from grandpa class
+
+        // L 9 in Father class is not present then
+        System.out.println(money);       // with -> (money); -> will access the parent on very first level of multi-level
+        System.out.println(super.money); // with -> (super.money); will -> access immediate parent class
+
+    }
+}
+
+class Tester{
+    public static void main(String[] args) {
+      Child child = new Child();
+
+      child.printMoney();
+    }
+}
