f7a49c7b4299de3a1819f241f601dfd3e78004c5
adhikari1983 <91963837+adhikari1983@users.noreply.github.com>
2023-06-03 17:35:25 -0400

    edits

--- src/main/java/class26/E4ConfigFiles.java
+++ src/main/java/class26/E4ConfigFiles.java
@@ -10,15 +10,20 @@ public class E4ConfigFiles {
         //e.g. System.currentTimeMillis();
         System.out.println(System.getProperty("user.dir"));
 
-        //we can use Absolute path as well ->F:\sdetJavaBatch16\Files\Config.properties
-        String path=System.getProperty("user.dir")+"\\Files\\Config.properties";
+        //we can also use Absolute path as well ->F:\sdetJavaBatch16\Files\Config.properties
+        // but to synchronized with other tester after pushing it on github
+        // it won't work to collaborate with other team member
+        // the base of path is defined according to generic System.getProperty("user.dir")
+        // & append the path from project, which is same for all team member
+        //So, L19 is generic for all team member.
+        String path = System.getProperty("user.dir") + "\\Files\\Config.properties";
         System.out.println(path);
 
         //passing the path to make stream of -> fileInputStream to load in properties further on
         FileInputStream fileInputStream = new FileInputStream(path);
 
         //This "Properties" -> special class which knows how this file works
-        // like the software tool to read the files
+        // it's like the software tool to read the files
         Properties properties = new Properties();
         properties.load(fileInputStream);
 
--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -1,9 +1,11 @@
 package utils;
 
 public class Constants {
+    //@static -> no object needed to call
+    // no object creation means saving the memory
+    public static final String CONFIG_READER_PATH =
+                                System.getProperty("user.dir") + "\\" + "Files\\Config.properties";
 
-    public static final String ConfigReaderPath =
-                        System.getProperty("user.dir") + "\\" + "Files\\Config.properties";
-                         // F:\sdetJavaBatch16\Files\Config.properties -> same as absolute path
-
+    public static final String EXCEL_FILE_PATH =
+                                System.getProperty("user.dir") + "\\Files\\Employees.xlsx";
 }
